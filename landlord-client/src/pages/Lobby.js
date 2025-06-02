0a08cd4f0ef9c4bdda83cfa2a54bf934bec0122a
Ivan Kwok <ivnkwok@gmail.com>
2025-06-01 17:26:30 -0700

    created basic lobby route

--- landlord-client/src/pages/Lobby.js
+++ landlord-client/src/pages/Lobby.js
@@ -1,2 +1,19 @@
 import React, {useState} from 'react';
+import { useParams } from 'react-router-dom';
 
+function Lobby() {
+
+    const {roomCode} = useParams();
+    function EndSession() {
+
+    }
+
+    return (
+        <div>
+            <button>End Session</button>
+            <p>{roomCode}</p>
+        </div>
+    )
+}
+
+export default Lobby;
