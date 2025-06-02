4e575e403bdcb158333587421d7dec6a00ca7155
Ivan Kwok <ivnkwok@gmail.com>
2025-06-01 17:18:46 -0700

    updated api call
    created username input

--- landlord-client/src/App.js
+++ landlord-client/src/App.js
@@ -1,46 +1,14 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const apiUrl = 'https://landlordcardgameapi-d4fkbke4ewdjbqcw.canadacentral-01.azurewebsites.net/session/createsession'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const [sessionID, setSessionID] = useState('');
-
-  const getSessionID = () => {
-    fetch(apiUrl).then(response => {
-      if (!response.ok) {
-        if (response.status == 404) {
-          throw new Error('URL not found');
-        } else if (response.status === 500) {
-          throw new Error('Server error');
-        }
-        else {
-          throw new Error('Network error');
-        }
-      }
-      return response.text()
-    })
-    .then(data => {
-      setSessionID(data)
-    })
-    .catch(error => {
-      console.error('Error: ', error)
-      setSessionID('Error: ', error)
-    })
-  } 
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{sessionID || 'no session yet'}</p>
-        <button onClick={getSessionID}>
-          Create new session
-        </button>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ landlord-client/src/pages/Home.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+const apiUrl = 'https://landlordcardgameapi-d4fkbke4ewdjbqcw.canadacentral-01.azurewebsites.net/session/createsession'
+
+function Home() {
+  const [sessionID, setSessionID] = useState('');
+  const [username, setUsername] = useState('Guest')
+
+  const getSessionID = () => {
+    fetch(`${apiUrl}?userName=${username}`, {
+      method: 'POST',
+    })
+    .then(response => {
+      if (!response.ok) {
+        if (response.status == 404) {
+          throw new Error('URL not found');
+        } else if (response.status === 500) {
+          throw new Error('Server error');
+        }
+        else {
+          throw new Error('Network error');
+        }
+      }
+      return response.text()
+    })
+    .then(data => {
+      setSessionID(data)
+    })
+    .catch(error => {
+      console.error('Error: ', error)
+      setSessionID('Error: ', error)
+    })
+  } 
+
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{sessionID || 'no session yet'}</p>
+        <input 
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button onClick={getSessionID}>
+          Create new session
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
